685db6345f295f8fd8e905958239d8f63da8dbba
Bithead942 <bithead942@gmail.com>
2024-09-12 13:24:54 -0400

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WDSendTextMsg")> 
+<Assembly: AssemblyDescription("Sends SMS text messages to owners")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("WdSendTextMsg")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1c080d96-7928-4043-bf31-8bd34f24ee2f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ WDSendTextMsg.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main(ByVal CmdArgs() As String)
+        Dim i As Integer
+        Dim mySMTPClient As SmtpClient = New SmtpClient
+        Dim strMessage As String
+        Dim Message As MailMessage = New MailMessage("Watchdog@VH.com", CmdArgs(0) & "@vtext.com")
+
+        Message.Subject = "SECURITY WARNING"
+        Message.Priority = MailPriority.High
+        strMessage = ""
+        mySMTPClient.Host = "smtp-server.cinci.rr.com"
+        mySMTPClient.Port = 25
+        If CmdArgs.Length > 0 Then
+            For i = 1 To (CmdArgs.Length - 1)
+                strMessage = strMessage & " " & CmdArgs(i)
+            Next i
+            Message.Body = strMessage
+            Try
+                mySMTPClient.Send(Message)
+            Catch ex As Exception
+                Exit Sub
+            End Try
+        End If
+    End Sub
+
+End Module
